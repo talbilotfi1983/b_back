564483327f9fcb1f5b29d341d5cfdc990cd346b7
GRP_AUTOVISION\ltalbi <ltalbi@autovision.fr>
2023-02-06 00:05:34 +0100

    update panier

---
+++ models/orderModel.js
@@ -0,0 +1,26 @@
+//commande
+const mongoose = require('mongoose');
+const orderSchema = new mongoose.Schema({
+        products: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId, ref: 'Product'
+                },
+                count: Number,
+                color: String,
+            }
+        ],
+        paymentIntent: {},
+
+        orderStatus: {
+            type: String,
+            default: 'Not proccessed',
+            enum: ["Not proccessed", 'Cash on delivry', 'Processing', 'Dispached', 'Cancelled', 'Delivered']
+        },
+    orderby :{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    }
+
+    }, {timestamps: true}
+);
+module.exports = mongoose.model("Order", orderSchema);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const {createUser, login, getUsers, getOneUser, deleteUser,forgotPasswordToken,resetPassword,
-    updateUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword} = require('../controller/userCtrl');
+const {
+    createUser, login, getUsers, getOneUser, deleteUser, forgotPasswordToken, resetPassword,
+    updateUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, getWishList,
+    saveUserAdress, addUserCart,getUserCart
+} = require('../controller/userCtrl');
 const {authMiddlewares, isAdmin, isBlocked} = require('../middlewares/authMiddlewares');
 const router = express.Router();
 router.post("/add", createUser);
@@ -8,12 +11,16 @@ router.post("/forgotPasswordToken", forgotPasswordToken);
 router.post("/resetPassword", forgotPasswordToken);
 router.post("/login", login);
 router.get("/logout", logout);
+router.post("/addUserCart", authMiddlewares, addUserCart);
+router.get("/getWishlist", authMiddlewares, getWishList);
+router.get("/getUserCart", authMiddlewares, getUserCart);
 router.get("/all", authMiddlewares, isAdmin, isBlocked, getUsers);
 router.get("/refresh", handleRefreshToken);
 router.get("/:id", authMiddlewares, isAdmin, isBlocked, getOneUser);
 router.delete("/:id", deleteUser);
 router.put("/resetPassword/:token", resetPassword);
 router.put("/updatePassword", authMiddlewares, updatePassword);
+router.put("/saveUserAdress", authMiddlewares, saveUserAdress);
 router.put("/:id", updateUser);
 router.put("/block/:id", authMiddlewares, isAdmin, isBlocked, blockUser);
 router.put("/unblock/:id", authMiddlewares, isAdmin, isBlocked, unblockUser);
